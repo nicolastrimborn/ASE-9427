2cbc3d69b0d543afa050ca5e97642ffb5d62f093
Ishira1601 <45150814+Ishira1601@users.noreply.github.com>
2019-03-31 20:22:56 +0300

    learned to publish

---
+++ ConveyorNow/java/fb/rt/mva/MECH_VIEWL.java
@@ -0,0 +1,115 @@
+/* Copyright (c)2019 Holobloc Inc. All rights reserved. */
+package fb.rt.mva; import fb.rt.net.*; import fb.rt.hmi.*;
+import fb.datatype.*;
+import fb.rt.*;
+import fb.rt.events.*;
+import fb.rt.hmi.*;
+import fb.rt.net.*;
+/** FUNCTION_BLOCK MECH_VIEWL (* Linear Mechanism View with Local Data Publish/Subscribe *)
+  * @author JHC
+  * @version 20190331/JHC - Generated.
+  */
+public class MECH_VIEWL extends FBMVCD {
+/** Initialized */
+public final EventOutput INITO = new EventOutput();
+/** Initialize */
+public final EventServer INIT = (e) -> service_INIT();
+/** FB VIEW:MECH_VIEW */
+  protected MECH_VIEW VIEW = new MECH_VIEW() ;
+/** FB UI:PUBL_1 */
+  protected PUBL UI = new PUBL(1);
+/** FB REND:SUBL_2 */
+  protected SUBL REND = new SUBL(2);
+/** Rendering Data Channel from Model */
+  public WSTRING REND_ID = new WSTRING();
+/** User Input Channel to Model */
+  public WSTRING UI_ID = new WSTRING();
+/** Background Color */
+  public COLOR BKGD = new COLOR("blue");
+/** Length in Diameters */
+  public UINT LEN = new UINT();
+/** Diameter */
+  public UINT DIA = new UINT();
+/** Orientation */
+  public VDIR DIR = new VDIR();
+/** The default constructor. */
+public MECH_VIEWL(){
+    super();
+    VIEW.INITO.connectTo(UI.INIT);
+    UI.INITO.connectTo(INITO);
+    VIEW.IND.connectTo(UI.REQ);
+    REND.INITO.connectTo(VIEW.INIT);
+    REND.IND.connectTo(VIEW.REQ);
+    UI.connectIVNoException("SD_1",VIEW.ovNamedNoException("FAULT"));
+    UI.connectIVNoException("ID",UI_ID);
+    VIEW.connectIVNoException("BKGD",BKGD);
+    VIEW.connectIVNoException("LEN",LEN);
+    VIEW.connectIVNoException("DIA",DIA);
+    VIEW.connectIVNoException("DIR",DIR);
+    REND.connectIVNoException("ID",REND_ID);
+    REND.connectOVNoException("RD_1",VIEW.POS);
+    REND.connectOVNoException("RD_2",VIEW.WKPC);
+    VIEW.STYLE.initializeNoException("VSTYLE#DISC");
+  }
+	/**
+ * {@inheritDoc}
+ * @param newVar {@inheritDoc}
+ */
+@Override
+protected void connectInternal(ANY newVar) {
+  if(newVar == UI_ID)
+    UI.connectIVNoException("ID",UI_ID);
+  if(newVar == BKGD)
+    VIEW.connectIVNoException("BKGD",BKGD);
+  if(newVar == LEN)
+    VIEW.connectIVNoException("LEN",LEN);
+  if(newVar == DIA)
+    VIEW.connectIVNoException("DIA",DIA);
+  if(newVar == DIR)
+    VIEW.connectIVNoException("DIR",DIR);
+  if(newVar == REND_ID)
+    REND.connectIVNoException("ID",REND_ID);
+}
+/** start the FB instances. */
+public void start( ){
+    super.start();
+  VIEW.start();
+  UI.start();
+  REND.start();
+}
+/** stop the FB instances. */
+public void stop( ){
+    super.stop();
+  VIEW.stop();
+  UI.stop();
+  REND.stop();
+}
+/** kill the FB instances. */
+public void kill( ){
+    super.kill();
+  VIEW.kill();
+  UI.kill();
+  REND.kill();
+}
+/** reset the FB instances. */
+public void reset( ){
+    super.reset();
+  VIEW.reset();
+  UI.reset();
+  REND.reset();
+}
+protected synchronized void service_INIT(){
+   REND.INIT.serviceEvent(this);
+}
+/** {@inheritDoc}
+ * @param fbName {@inheritDoc}
+ * @param r {@inheritDoc}
+ * @throws FBRManagementException {@inheritDoc} */
+  public void initialize(String fbName, Resource r)
+  throws FBRManagementException{
+    super.initialize(fbName,r);
+    VIEW.initialize("VIEW",r);
+    UI.initialize("UI",r);
+    REND.initialize("REND",r);
+}
+}
---
+++ ConveyorNow/java/fb/rt/template/Controller.java
@@ -0,0 +1,151 @@
+/* Copyright (c)2019 Holobloc Inc. All rights reserved. */
+package fb.rt.template;
+import fb.datatype.*;
+import fb.rt.*;
+import fb.rt.events.*;
+/** FUNCTION_BLOCK Controller (* Basic Function Block Type *)
+  * @author JHC
+  * @version 20190331/JHC - Generated.
+  */
+public class Controller extends fb.rt.FBInstance {
+/** The index (0) of state START. */
+public static final int INDEX_START = 0;
+/** The index (1) of state INIT. */
+public static final int INDEX_INIT = 1;
+/** The index (2) of state CLK. */
+public static final int INDEX_CLK = 2;
+/** The index (3) of state LD. */
+public static final int INDEX_LD = 3;
+/** Initialization Confirm */
+public final EventOutput INITO = new EventOutput();
+/** EVENT CNF */
+public final EventOutput CNF = new EventOutput();
+/** Initialization Request */
+public final EventServer INIT = (e) -> service_INIT();
+/** Normal Execution Request */
+public final EventServer CLK = (e) -> service_CLK();
+/** EVENT LD */
+public final EventServer LD = (e) -> service_LD();
+/** VAR GREENTIME:UINT */
+  public UINT GREENTIME = new UINT();
+/** VAR MINGREENTIME:UINT */
+  public UINT MINGREENTIME = new UINT();
+/** VAR PEDCROSS:BOOL */
+  public BOOL PEDCROSS = new BOOL();
+/** VAR YELLOWTIME:UINT */
+  public UINT YELLOWTIME = new UINT();
+/** Output event qualifier */
+  public final BOOL RED = new BOOL();
+/** VAR YELLOW:BOOL */
+  public final BOOL YELLOW = new BOOL();
+/** VAR GREEN:BOOL */
+  public final BOOL GREEN = new BOOL();
+/** VAR RT:UINT */
+  public final UINT RT = new UINT();
+/** VAR RELEASE:BOOL */
+  public final BOOL RELEASE = new BOOL();
+/** VAR MINGREENTIMEIN:UINT */
+  public UINT MINGREENTIMEIN = new UINT();
+/** VAR YELLOWTIMEIN:UINT */
+  public UINT YELLOWTIMEIN = new UINT();
+/** VAR GREENTIMEIN:UINT */
+  public UINT GREENTIMEIN = new UINT();
+/** The default constructor. */
+public Controller(){
+    super();
+  }
+protected synchronized void service_INIT(){
+  if(eccState == INDEX_START){
+    state_INIT();
+  }
+}
+protected synchronized void service_CLK(){
+  if(eccState == INDEX_START){
+    state_CLK();
+  }
+}
+protected synchronized void service_LD(){
+  if(eccState == INDEX_START){
+    state_LD();
+  }
+}
+/** The actions to take upon entering state START. */
+void state_START(){
+   eccState = INDEX_START;
+}
+/** The actions to take upon entering state INIT. */
+void state_INIT(){
+   eccState = INDEX_INIT;
+   alg_INIT();
+   INITO.serviceEvent(this);
+   state_START();
+}
+/** The actions to take upon entering state CLK. */
+void state_CLK(){
+   eccState = INDEX_CLK;
+   alg_CLK();
+   CNF.serviceEvent(this);
+   state_START();
+}
+/** The actions to take upon entering state LD. */
+void state_LD(){
+   eccState = INDEX_LD;
+   alg_LD();
+   CNF.serviceEvent(this);
+   state_START();
+}
+  /** ALGORITHM INIT IN ST*/
+public void alg_INIT(){
+RED.value=false;
+YELLOW.value=false;
+GREEN.value=false;
+MINGREENTIMEIN.value=100;
+YELLOWTIMEIN.value=100;
+GREENTIMEIN.value=100;
+RELEASE.value=false;}
+  /** ALGORITHM CLK IN ST*/
+public void alg_CLK(){
+if( PEDCROSS.value ){
+	if( MINGREENTIMEIN.value<=0 ){
+ 		GREEN.value=false;
+ 		YELLOW.value=true;
+ 		RED.value=false;
+ 		GREENTIMEIN.value=0;
+	}
+}
+if( GREENTIMEIN.value>0 ){
+   GREENTIMEIN.value = GREENTIMEIN.value - 1;
+   MINGREENTIMEIN.value= MINGREENTIMEIN.value - 1;
+   GREEN.value=true;
+   YELLOW.value= false;
+   RED.value=false;
+   RT.value=GREENTIMEIN.value;
+}
+
+if( GREENTIMEIN.value==0 ){
+    GREEN.value=false;
+	YELLOW.value= true;
+    RED.value=false;
+	if( YELLOWTIMEIN.value>0 ){
+		YELLOWTIMEIN.value= YELLOWTIMEIN.value - 1;
+        RT.value=YELLOWTIMEIN.value;
+	}
+if( YELLOWTIMEIN.value==0 ){
+     GREEN.value=false;
+     YELLOW.value=false;
+	 RED.value=true;
+	 RELEASE.value=true;
+     RT.value=0;
+	}
+}}
+  /** ALGORITHM LD IN ST*/
+public void alg_LD(){
+GREEN.value=true;
+YELLOW.value=false;
+RED.value=false;
+MINGREENTIMEIN.value=MINGREENTIME.value;
+YELLOWTIMEIN.value=YELLOWTIME.value+1;
+GREENTIMEIN.value=GREENTIME.value;
+RT.value=GREENTIME.value;
+RELEASE.value=false;}
+}
